a1970b5fe6d750c5168c45f834e9376cfe79e956
MishkaSimakov <msimakov661@gmail.com>
2019-01-11 22:16:06 +0300

    second commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,7 @@ use function compact;
 use function GuzzleHttp\Promise\all;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Khill\Lavacharts\Lavacharts;
 use function max;
 use function min;
 use function var_dump;
@@ -35,6 +36,28 @@ class PostController extends Controller
             $archives = Archive::all()->where('channel_id', $channel->id);
         }
 
-        return view('posts.show', compact('archives', 'post'));
+        $lava = new Lavacharts; // See note below for Laravel
+
+        $population = $lava->DataTable();
+
+        $population->addDateColumn('Year')->addNumberColumn('Number of People')
+            ->addRow(['2006', 623452])
+            ->addRow(['2007', 685034])
+            ->addRow(['2008', 716845])
+            ->addRow(['2009', 757254])
+            ->addRow(['2010', 778034])
+            ->addRow(['2011', 792353])
+            ->addRow(['2012', 839657])
+            ->addRow(['2013', 842367])
+            ->addRow(['2014', 873490]);
+
+        $lava->AreaChart('Population', $population, [
+            'title' => 'Population Growth',
+            'legend' => [
+                'position' => 'in'
+            ]
+        ]);
+
+        return view('posts.show', compact('archives', 'post', 'population', 'lava'));
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,12 @@
 <h2>Просмотр значений</h2>
 
 @foreach($archives as $archive)
-<a href="{{ $archive->channel->url }}"><h2>{{ $archive->channel->sensor->name }}:  {{ $archive->value }} {{ $archive->channel->sensor->units }}</h2></a><time title="{{ $archive->date }}">{{ date('H', $archive->date) }}</time>
+<a href="{{ $archive->channel->url }}"><h2>{{ $archive->channel->sensor->name }}:  {{ $archive->value }} {{ $archive->channel->sensor->units }}</h2></a>
+<time title="{{ $archive->date }}">{{ $archive->date }}</time>
 @endforeach
+
+<div id="pop_div"></div>
+
+<?= $lava->render('AreaChart', 'Population', 'pop_div') ?>
 
 @include('partials.footer')
